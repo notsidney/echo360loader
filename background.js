f84d49769bb7d183b243b3cbb93fec075d2bd971
Sidney Alcantara <sidneyalcantara@outlook.com.au>
2018-10-17 02:07:45 +1100

    basic extension

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'developer.chrome.com'},
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
